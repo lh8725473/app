069bc641ee869972997b79ed7a15f72139e8d028
lh8725473 <171213574@qq.com>
2014-07-15 12:01:27 +0800

    delete user server

--- src/app/resources/users.js
+++ src/app/resources/users.js
@@ -29,33 +29,3 @@ angular.module('App.Resources').factory('Users', function($resource, CONFIG) {
         }
     })
 })
-angular.module('App.Resources').factory('Users', function($resource, CONFIG) {
-    return $resource(CONFIG.API_ROOT + '/user/:action/:id', {}, {
-        query: {
-        	method: "GET",
-            params: {
-                action: 'list'
-            }
-        },
-        delete: {
-        	method: "DELETE",
-            params: {
-                action: 'delete',
-                id : 0
-            }
-        },
-        create: {
-        	method: "POST",
-            params: {
-                action: 'create',
-            }
-        },
-        update: {
-        	method: "PUT",
-            params: {
-                action: 'update',
-                id : 0
-            }
-        }
-    })
-})
