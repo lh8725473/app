90e9f19eea9d9aa07ca3a2c87f4af9c45576bf77
lh8725473 <171213574@qq.com>
2014-07-28 17:32:51 +0800

    go on theme

--- src/app/sidebar/controller.js
+++ src/app/sidebar/controller.js
@@ -1,2 +1,3 @@
-angular.module('App.Sidebar').controller('App.Sidebar.Controller', function ($scope) {
+angular.module('App.Sidebar').controller('App.Sidebar.Controller', function ($scope, $rootScope, $location) {
+	$rootScope.location = $location;
 })
