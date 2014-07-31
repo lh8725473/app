23365a81a080879a5f5fd9bb9ee373e17c6243e6
island205 <island205@gmail.com>
2014-07-31 22:23:19 +0800

    fix fucky grid

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,6 @@
 angular.module('App.Config', []).constant('CONFIG', {
-//API_ROOT: 'http://101.231.216.75:8888/api',
-  API_ROOT: 'http://localhost/api',
+  API_ROOT: 'http://101.231.216.75:8888/api',
+  // API_ROOT: 'http://localhost/api',
   // API_ROOT: 'http://10.10.10.57/api',
   token: 'b7b35a2a8d38c4edf4bf176de12722a9'
   // token: 'f9ddda3d733345001511a8825e6c9f4f'
--- src/app/users/managedUsers/controller.js
+++ src/app/users/managedUsers/controller.js
@@ -1,261 +1,272 @@
 angular.module('App.Users.ManagedUsers').controller('App.Users.ManagedUsers.Controller', function(
-    $scope,
-    $modal,
-    Notification,
-    Users,
-    Group) {
+  $scope,
+  $modal,
+  Notification,
+  Users,
+  Group) {
 
-    //addUser window
-    $scope.addUser = function() {
-        var addUserModal = $modal.open({
-            templateUrl: 'src/app/users/managedUsers/add-user-modal.html',
-            windowClass: 'add-user-modal-view',
-            backdrop : 'static',
-            controller: addUserModalController,
-            resolve: {
-                groupList: function() {
-                    return Group.query()
-                }
-            }
-        })
-
-        addUserModal.result.then(function(user) {
-            Users.create({}, user).$promise.then(function(resUser) {
-                $scope.userList.push(resUser)
-                Notification.show({
-                    title: '成功',
-                    type: 'success',
-                    msg: '添加用户成功',
-                    closeable: true
-                })
-            }, function (error) {
-                Notification.show({
-                    title: '失败',
-                    type: 'danger',
-                    msg: error.data.result,
-                    closeable: false
-                })
-            })
-        })
-    }
-
-    //addUser window ctrl
-    var addUserModalController = function($scope, $modalInstance, groupList) {
-    	$scope.groupList = groupList;
-    	$scope.selectedData = [];
-    	$scope.gridGroup = {
-        	data: 'groupList',
-        	selectedItems: $scope.selectedData,
-//      	enableRowSelection : false,
-        	showSelectionCheckbox: true,
-        	selectWithCheckboxOnly: true,
-    		showSelectionCheckbox: true,
-        	afterSelectionChange: function(data) {
-        		if(data.entity.showRoleMenu == true){
-        			data.entity.showRoleMenu = false;
-        		}else{
-        			data.entity.showRoleMenu = true;
-        		}
-			},
-        	columnDefs: [{
-                	field: 'group_name',
-                	displayName: '群组名称'
-            	}, {
-                	field: 'group_desc',
-                	displayName: '人数'
-            	}, {
-                	displayName: '组内权限',
-                	cellTemplate: 'src/app/users/managedUsers/row-group-role.html',
-            	}         	
-        	]
-   	 	}
-
-    	$scope.seachGroups = function(seachGroupsValue){
-    		$scope.groupList = $(groupList).map(function(i, v) {
-				if (v.group_name.toLowerCase().indexOf(seachGroupsValue.toLowerCase()) != -1) {
-					return v
-				}
-			}).get();
-			//add selected element 
-			var temGroupList = $scope.groupList;
-			angular.forEach($scope.selectedData, function (selectedGroup) {
-				angular.forEach(temGroupList, function (group) {
-        			if (group.group_id != selectedGroup.group_id) {
-        				$scope.groupList.push(selectedGroup)
-        			}
-        		})
-        	})
-			//remove repeat element
-			$scope.groupList = $.unique($scope.groupList);
-    	}
-    	
-        groupList.$promise.then(function() {
-            angular.forEach(groupList, function(group) {
-                group.showRoleMenu = false;
-                group.groupRole = 1;
-            });
-        });
-        $scope.groupList = groupList;
-        $scope.showAccountAdmin = false;
-
-        $scope.switchAccountAdmin = function() {
-            $scope.showAccountAdmin = !$scope.showAccountAdmin;
-        };
-
-        $scope.switchRoleMenu = function(group) {
-            group.showRoleMenu = !group.showRoleMenu;
-        };
-
-        $scope.ok = function(user) {
-        	var groups = []
-        	angular.forEach(groupList, function (group) {
-        		if (group.showRoleMenu) {
-        			groups.push({
-        				group_id: group.group_id,
-        				group_role: group.groupRole
-        			})
-        		}
-        	})
-        	user.groups = groups;
-            $modalInstance.close(user)
+  //addUser window
+  $scope.addUser = function() {
+    var addUserModal = $modal.open({
+      templateUrl: 'src/app/users/managedUsers/add-user-modal.html',
+      windowClass: 'add-user-modal-view',
+      backdrop: 'static',
+      controller: addUserModalController,
+      resolve: {
+        groupList: function() {
+          return Group.query()
         }
+      }
+    })
 
-        $scope.cancel = function() {
-            $modalInstance.dismiss('cancel')
-        }
-    }
-
-    //userList data
-    $scope.userList = Users.query()
-
-    $scope.gridOptions = {
-        data: 'userList',
-        selectedItems: [],
-        headerRowHeight: 36,
-        rowHeight: 60,
-        enableRowSelection : false,
-//      showSelectionCheckbox: true,
-        columnDefs: [{
-                displayName : '用户',
-                cellTemplate: 'src/app/users/managedUsers/row-user-name.html',
-                cellClass: 'grid-align'
-            }, {
-                field: 'email',
-                displayName: '邮箱'
-            }, {
-                field: 'space',
-                displayName: '用量'
-            }, {
-                cellTemplate: 'src/app/users/managedUsers/row-user-activety.html',
-                displayName: '活动'
-            }, {
-                cellTemplate: 'src/app/users/managedUsers/user-table-action-cell.html',
-                displayName: '更多'
-            }
-        ]
-    }
-
-    //deleteUser
-    $scope['delete'] = function(row) {
-        console.log("Here I need to know which row was selected " + row.entity.user_id)
-        var deleteUserModal = $modal.open({
-            templateUrl: 'src/app/users/managedUsers/delete-user-modal.html',
-            controller: deleteModalController,
-            resolve: {
-                userId: function() {
-                    return row.entity.user_id
-                }
-            }
+    addUserModal.result.then(function(user) {
+      Users.create({}, user).$promise.then(function(resUser) {
+        $scope.userList.push(resUser)
+        Notification.show({
+          title: '成功',
+          type: 'success',
+          msg: '添加用户成功',
+          closeable: true
         })
-
-        deleteUserModal.result.then(function(userId) {
-            Users['delete']({
-                id: userId
-            }).$promise.then(function() {
-                for (var i = 0; i < $scope.userList.length; ++i) {
-                    if ($scope.userList[i].user_id == userId) break
-                }
-                $scope.userList.splice(i, 1)
-                Notification.show({
-                    title: '成功',
-                    type: 'success',
-                    msg: '删除用户成功',
-                    closeable: true
-                })
-            })
+      }, function(error) {
+        Notification.show({
+          title: '失败',
+          type: 'danger',
+          msg: error.data.result,
+          closeable: false
         })
-    }
+      })
+    })
+  }
 
-    //delete window ctrl
-    var deleteModalController = function($scope, $modalInstance, userId) {
-        $scope.ok = function() {
-            $modalInstance.close(userId)
+  //addUser window ctrl
+  var addUserModalController = function($scope, $modalInstance, groupList) {
+    $scope.groupList = groupList;
+    // 过滤后的数据
+    $scope.shownData = [];
+    // 选中的数据
+    $scope.selectedData = [];
+    $scope.gridGroup = {
+      data: 'shownData',
+      selectedItems: $scope.selectedData,
+      // enableRowSelection : false,
+      showSelectionCheckbox: true,
+      selectWithCheckboxOnly: true,
+      showSelectionCheckbox: true,
+      afterSelectionChange: function(rows, checkAll) {
+        if (!angular.isArray(rows)) {
+          rows = [rows]
         }
-
-        $scope.cancel = function() {
-            $modalInstance.dismiss('cancel')
-        }
-    }
-
-    //editUser
-    $scope.edit = function edit(row) {
-        console.log("Here I need to know which row was selected " + row.entity.user_id)
-        var editUserModal = $modal.open({
-            templateUrl: 'src/app/users/managedUsers/update-user-modal.html',
-            controller: editModalController,
-            resolve: {
-                editUser: function() {
-                    // Past the ref to the modal
-                    return angular.copy(row.entity)
-                }
-            }
+        angular.forEach(rows, function(row) {
+          if (angular.isUndefined(checkAll)) {
+            row.entity.showRoleMenu = !row.entity.showRoleMenu;
+          } else {
+            row.entity.showRoleMenu = checkAll;
+          }
         })
-
-        editUserModal.result.then(function(editUser) {
-            Users.update({
-                id: editUser.user_id
-            }, editUser).$promise.then(function() {
-                angular.extend(row.entity, editUser)
-                Notification.show({
-                    title: '成功',
-                    type: 'success',
-                    msg: '修改用户成功',
-                    closeable: true
-                })
-            }, function (error) {
-                Notification.show({
-                    title: '失败',
-                    type: 'danger',
-                    msg: error.data.result,
-                    closeable: true
-                })
-            })
+        // 重新计算选中的项
+        $scope.selectedData = $($scope.groupList).filter(function(index, group) {
+          return group.showRoleMenu;
         })
+      },
+      columnDefs: [{
+        field: 'group_name',
+        displayName: '群组名称'
+      }, {
+        field: 'group_desc',
+        displayName: '人数'
+      }, {
+        displayName: '组内权限',
+        cellTemplate: 'src/app/users/managedUsers/row-group-role.html',
+      }]
     }
 
-    //edit window ctrl
-    var editModalController = function($scope, $modalInstance, editUser) {
-        $scope.editUser = editUser
-
-        $scope.ok = function() {
-            $modalInstance.close($scope.editUser)
+    $scope.seachGroups = function(seachGroupsValue) {
+      // 清空显示的group
+      $scope.shownData = []
+      // 重新计算
+      $scope.shownData = $($scope.groupList).filter(function(index, group) {
+        if (!seachGroupsValue || seachGroupsValue.trim() === '') {
+          return true
+        } else if (group.group_name.toLowerCase().indexOf(seachGroupsValue.toLowerCase()) != -1) {
+          return true
+        } else {
+          return false
         }
+      })
+    }
 
-        $scope.cancel = function() {
-            $modalInstance.dismiss('cancel')
+    groupList.$promise.then(function() {
+      angular.forEach(groupList, function(group) {
+        group.showRoleMenu = false;
+        group.groupRole = 1;
+        // 获取数据之后，全部填充到显示的数据中
+        $scope.shownData.push(group);
+      });
+
+    });
+    $scope.groupList = groupList;
+    $scope.showAccountAdmin = false;
+
+    $scope.switchAccountAdmin = function() {
+      $scope.showAccountAdmin = !$scope.showAccountAdmin;
+    };
+
+    $scope.switchRoleMenu = function(group) {
+      group.showRoleMenu = !group.showRoleMenu;
+    };
+
+    $scope.ok = function(user) {
+      var groups = []
+        // TODO 我觉得这里的逻辑完全是交互的问题，用户本来选了的，但是搜索的时候影藏了还要不要添加进去呢？
+      angular.forEach(groupList, function(group) {
+        if (group.showRoleMenu) {
+          // or  if (group.showRoleMenu && gorup.show) {
+          groups.push({
+            group_id: group.group_id,
+            group_role: group.groupRole
+          })
         }
+      })
+      user.groups = groups;
+      $modalInstance.close(user)
     }
 
-    $scope.bulkEdit = function() {
-        alert("bulkEdit")
+    $scope.cancel = function() {
+      $modalInstance.dismiss('cancel')
+    }
+  }
+
+  //userList data
+  $scope.userList = Users.query()
+
+  $scope.gridOptions = {
+    data: 'userList',
+    selectedItems: [],
+    headerRowHeight: 36,
+    rowHeight: 60,
+    enableRowSelection: false,
+    //      showSelectionCheckbox: true,
+    columnDefs: [{
+      displayName: '用户',
+      cellTemplate: 'src/app/users/managedUsers/row-user-name.html',
+      cellClass: 'grid-align'
+    }, {
+      field: 'email',
+      displayName: '邮箱'
+    }, {
+      field: 'space',
+      displayName: '用量'
+    }, {
+      cellTemplate: 'src/app/users/managedUsers/row-user-activety.html',
+      displayName: '活动'
+    }, {
+      cellTemplate: 'src/app/users/managedUsers/user-table-action-cell.html',
+      displayName: '更多'
+    }]
+  }
+
+  //deleteUser
+  $scope['delete'] = function(row) {
+    console.log("Here I need to know which row was selected " + row.entity.user_id)
+    var deleteUserModal = $modal.open({
+      templateUrl: 'src/app/users/managedUsers/delete-user-modal.html',
+      controller: deleteModalController,
+      resolve: {
+        userId: function() {
+          return row.entity.user_id
+        }
+      }
+    })
+
+    deleteUserModal.result.then(function(userId) {
+      Users['delete']({
+        id: userId
+      }).$promise.then(function() {
+        for (var i = 0; i < $scope.userList.length; ++i) {
+          if ($scope.userList[i].user_id == userId) break
+        }
+        $scope.userList.splice(i, 1)
+        Notification.show({
+          title: '成功',
+          type: 'success',
+          msg: '删除用户成功',
+          closeable: true
+        })
+      })
+    })
+  }
+
+  //delete window ctrl
+  var deleteModalController = function($scope, $modalInstance, userId) {
+    $scope.ok = function() {
+      $modalInstance.close(userId)
     }
 
-    $scope.bulkadd = function() {
-        alert("bulkadd")
+    $scope.cancel = function() {
+      $modalInstance.dismiss('cancel')
+    }
+  }
+
+  //editUser
+  $scope.edit = function edit(row) {
+    console.log("Here I need to know which row was selected " + row.entity.user_id)
+    var editUserModal = $modal.open({
+      templateUrl: 'src/app/users/managedUsers/update-user-modal.html',
+      controller: editModalController,
+      resolve: {
+        editUser: function() {
+          // Past the ref to the modal
+          return angular.copy(row.entity)
+        }
+      }
+    })
+
+    editUserModal.result.then(function(editUser) {
+      Users.update({
+        id: editUser.user_id
+      }, editUser).$promise.then(function() {
+        angular.extend(row.entity, editUser)
+        Notification.show({
+          title: '成功',
+          type: 'success',
+          msg: '修改用户成功',
+          closeable: true
+        })
+      }, function(error) {
+        Notification.show({
+          title: '失败',
+          type: 'danger',
+          msg: error.data.result,
+          closeable: true
+        })
+      })
+    })
+  }
+
+  //edit window ctrl
+  var editModalController = function($scope, $modalInstance, editUser) {
+    $scope.editUser = editUser
+
+    $scope.ok = function() {
+      $modalInstance.close($scope.editUser)
     }
 
-    $scope.exportUser = function() {
-        alert("exportUser")
+    $scope.cancel = function() {
+      $modalInstance.dismiss('cancel')
     }
+  }
+
+  $scope.bulkEdit = function() {
+    alert("bulkEdit")
+  }
+
+  $scope.bulkadd = function() {
+    alert("bulkadd")
+  }
+
+  $scope.exportUser = function() {
+    alert("exportUser")
+  }
 
 })
