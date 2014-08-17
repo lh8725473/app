a86b060d31df58e1150630dd7ff1d4ab54afd21f
lh8725473 <171213574@qq.com>
2014-08-17 15:44:01 +0800

    add msg couter

--- src/app/header/controller.js
+++ src/app/header/controller.js
@@ -4,16 +4,38 @@ angular.module('App.Header').controller('App.Header.Controller', [
   'CONFIG',
   'Users',
   '$cookies',
+  'Message',
+  '$timeout',
   function(
     $scope,
     $translatePartialLoader,
     CONFIG,
     Users,
-    $cookies
+    $cookies,
+    Message,
+    $timeout
   ) {
   	$scope.backToindex = function(){
   	  window.location.href = CONFIG.LOGIN_PATH
   	}
+  	
+  	$scope.messageCount = 0
+  	$scope.noticeCount = 0
+  	$scope.showMessageCount = !!$scope.messageCount
+  	$scope.showMoticeCount = !!$scope.noticeCount
+  	
+  	$scope.pollForMessages = function(){
+  		$scope.unreadCount = Message.getUnreadMessagesCount()
+  		$scope.unreadCount.$promise.then(function(){
+  			$scope.messageCount = $scope.unreadCount.letter
+  			$scope.noticeCount = $scope.unreadCount.notice
+			$scope.showMessageCount = $scope.messageCount != 0
+			$scope.showMoticeCount = $scope.noticeCount != 0
+  		})
+  		$timeout($scope.pollForMessages, 10000)
+  	}
+  	
+  	$scope.pollForMessages()
   	
   	$scope.id = $cookies.userId
     	
