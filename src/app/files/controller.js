fb889552090fbeee6d9566a1aa797a1c44815a4e
island205 <island205@gmail.com>
2014-08-17 15:26:45 +0800

    read file list

--- src/app/files/controller.js
+++ src/app/files/controller.js
@@ -1,9 +1,17 @@
 angular.module('App.Files').controller('App.Files.Controller', [
   '$scope',
+  '$state',
   'CONFIG',
+  'Folders',
   function(
     $scope,
-    CONFIG
+    $state,
+    CONFIG,
+    Folders
   ) {
-  }
-])
+    //userList data
+    var folderId = $state.params.folderId || 0;
+    $scope.objList = Folders.getObjList({
+      folder_id: folderId
+    })
+}])
